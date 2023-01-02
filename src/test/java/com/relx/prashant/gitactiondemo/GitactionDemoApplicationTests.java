e63383fb7a3be80361f0b3d7dfbdaf5cbe42309f
prashantmali <maliprashant1911@gmail.com>
2023-01-02 17:13:34 +0530

    first commit

---
+++ src/test/java/com/relx/prashant/gitactiondemo/GitactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relx.prashant.gitactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
